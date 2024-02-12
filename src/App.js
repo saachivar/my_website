d714b968427c6e3139b9364a3b8453c18118613f
Saachi Varshney <saachi.varshney@gmail.com>
2024-02-12 13:58:48 -0500

    just doing things

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Your m;kjom is a whore 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <div className="content">
+        <h1>hello my name is saachiiiii i spelled my anme wrong</h1>
+      </div>
     </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+const Sidebar = () => {
+    return (  
+        <nav className="sidebar">
+            <h1> yello</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">New Blog</a>
+            </div>
+        </nav>
+        
+    );
+}
+ 
+export default Sidebar;
