45ce9d7e723f5a1c20d1870216bf5a19b307b424
Saachi Varshney <saachi.varshney@gmail.com>
2024-02-19 14:06:21 -0500

    description change

--- src/Components/Descrip1.js
+++ src/Components/Descrip1.js
@@ -5,29 +5,27 @@ function Descrip1() {
         const slideInDiv = document.getElementById('descrip1-slide-in');
 
         const slideInOnLoad = () => {
-        slideInDiv.style.left = '0'; // Move the div to its final position when the component mounts
-      }; // Empty dependency array ensures the effect runs only once after the component mounts
+            slideInDiv.style.left = '0'; // Move the div to its final position when the component mounts
+        }; // Empty dependency array ensures the effect runs only once after the component mounts
 
-      // Add event listener to trigger slide-in animation when the page loads
-    window.addEventListener('load', slideInOnLoad);
+        // Add event listener to trigger slide-in animation when the page loads
+        window.addEventListener('load', slideInOnLoad);
 
-    // Cleanup: remove event listener when component unmounts
-    return () => {
-      window.removeEventListener('load', slideInOnLoad);
-    };
-  }, []); // Empty dependency array ensures the effect runs only once after the component mounts
-
-
+        // Cleanup: remove event listener when component unmounts
+        return () => {
+            window.removeEventListener('load', slideInOnLoad);
+        };
+    }, []); // Empty dependency array ensures the effect runs only once after the component mounts
 
 
       
     
-return (
-    <div className="Descrip1" id="descrip1-slide-in">
-        <p>I’m a first year student computer science major at the University of Georgia.</p>
-        <p>Scroll down to find out more.</p>
-        </div>
-    )
+    return (
+        <div className="Descrip1" id="descrip1-slide-in">
+            <p>I’m a first year student computer science major at the University of Georgia.</p>
+            <p>Scroll down to find out more.</p>
+            </div>
+        )
 
 }
 export default Descrip1;
