47f69915ce8fca8c75aefd4186d6e08247a702a4
Saachi Varshney <saachi.varshney@gmail.com>
2024-02-18 22:16:20 -0500

    helloooo

--- src/Components/About3.js
+++ src/Components/About3.js
@@ -2,7 +2,7 @@
 function About3() {
     return (
         <div className="about about3">
-            <p>I’m a first year student computer science major at the University of Georgia.</p>
+            <p>somethng elselkafjse</p>
             </div>
         )
     
