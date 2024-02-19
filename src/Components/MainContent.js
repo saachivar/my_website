c666ad0211fbdfcf53d23c7f06d5d2d3bd0ff5f4
Saachi Varshney <saachi.varshney@gmail.com>
2024-02-19 01:27:09 -0500

    more stuffff

--- src/Components/MainContent.js
+++ src/Components/MainContent.js
@@ -3,6 +3,15 @@ import Descrip1 from "./Descrip1.js"
 import About1 from "./About1.js";
 import About2 from "./About2.js";
 import About3 from "./About3.js";
+import ProjectDescrip from "./ProjectDescrip.js";
+
+const heading = 'Links Section';
+const links = [
+        { text: 'Link 1', url: '/link1' },
+        { text: 'Link 2', url: '/link2' },
+        { text: 'Link 3', url: '/link3' }
+    ];
+
 function MainContent() {
 return (
     <div className = "MainContent">
@@ -11,9 +20,9 @@ return (
         <About1 />
         <About2 />
         <About3 />
-
+        <ProjectDescrip heading={heading} links={links} />
         </div>
-)
+);
 
 }
 export default MainContent;
