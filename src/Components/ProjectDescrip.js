b380e1b9c2cbe0bbb300c4ffe280cd64039342e3
Saachi Varshney <saachi.varshney@gmail.com>
2024-02-19 01:27:01 -0500

    stuff

--- src/Components/ProjectDescrip.js
+++ src/Components/ProjectDescrip.js
@@ -1,10 +1,19 @@
-
-function ProjectDescrip() {
+// Child Component
+const ProjectDescrip = ({ heading, links }) => {
     return (
         <div className="ProjectDescrip">
-          <h3>   </h3>
-            </div>
-        )
-    
-    }
-    export default ProjectDescrip;
+            <h2> HELLO </h2>
+            <h2>{heading}</h2>
+            <ul>
+                {links.map((link, index) => (
+                    <li key={index}>
+                        <a href={link.url}>{link.text}</a>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+
+export default ProjectDescrip;
