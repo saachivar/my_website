c28e8c45da74cceb84e3f07323d52c505cd2f042
Saachi Varshney <saachi.varshney@gmail.com>
2024-03-07 12:23:21 -0500

    jkc;jkasjdkafd

--- src/Components/SidebarContent.js
+++ src/Components/SidebarContent.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import "../App.css";
+import "../images/Picture1.png"
+import "../images/Picture2.png"
+
 
 function SidebarContent() {
     const [scrollPosition, setScrollPosition] = useState(0);
@@ -24,23 +27,56 @@ function SidebarContent() {
         <div className="SidebarContent">
             <div className="title">saachily.com</div>
             <div className="sidebarElement">
+
                 <button onClick={() => scrollToHeight(0)} className="meTag sidebarElement " >
-                    {scrollPosition < window.innerHeight * 1 ? <p>  ⌄ me </p> : <p> > me</p>}
+                    {scrollPosition < window.innerHeight * 1 ? (
+                        <div className="scrollArrow"> <img src={ require ('../images/Picture1.png')} alt="something" width="10"/>
+                        <p> me </p> 
+                        </div>
+                        ) : (
+                        <div className="scrollArrow"> <img src={ require ('../images/Picture2.png')} alt="something" width="19"/>
+                        <p> me </p>
+                        </div>
+                        )}
                     </button>
             </div>
             <div className="sidebarElement">
                 <button onClick={() => scrollToHeight(1)} className="aboutTag sidebarElement ">
-                    {scrollPosition < window.innerHeight * 2 ? <p>  ⌄ about </p> : <p> > about</p>}
+                    {scrollPosition < window.innerHeight * 2 ? (
+                        <div className="scrollArrow"> <img src={ require ('../images/Picture1.png')} alt="something" width="10"/>
+                        <p> about </p> 
+                        </div>
+                        ) : (
+                        <div className="scrollArrow"> <img src={ require ('../images/Picture2.png')} alt="something" width="19"/>
+                        <p> about </p>
+                        </div>
+                        )}
                 </button>
             </div>
             <div className="sidebarElement">
                 <button onClick={() => scrollToHeight(2)} className="projectsTag sidebarElement ">
-                {scrollPosition < window.innerHeight * 3 ? <p>  ⌄ projects </p> : <p> > projects</p>}
+                {scrollPosition < window.innerHeight * 3 ? (
+                        <div className="scrollArrow"> <img src={ require ('../images/Picture1.png')} alt="something" width="10"/>
+                        <p> projects </p> 
+                        </div>
+                        ) : (
+                        <div className="scrollArrow"> <img src={ require ('../images/Picture2.png')} alt="something" width="19"/>
+                        <p> projects </p>
+                        </div>
+                        )}
                 </button>
             </div>
             <div className="sidebarElement">
                 <button onClick={() => scrollToHeight(3)} className="projectsTag sidebarElement ">
-                {scrollPosition < window.innerHeight * 4 ? <p>  ⌄ contact </p> : <p> > contact</p>}
+                {scrollPosition < window.innerHeight * 4 ? (
+                        <div className="scrollArrow"> <img src={ require ('../images/Picture1.png')} alt="something" width="10"/>
+                        <p> contact </p> 
+                        </div>
+                        ) : (
+                        <div className="scrollArrow"> <img src={ require ('../images/Picture2.png')} alt="something" width="19"/>
+                        <p> contact </p>
+                        </div>
+                        )}
                 </button>
             </div>
         </div>
