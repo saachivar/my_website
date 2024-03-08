e444c868a6b62402ba9077f16b10c2d430461168
Saachi Varshney <saachi.varshney@gmail.com>
2024-03-08 14:19:12 -0500

    sugarcoat dododododo

--- src/Components/ProjectDescrip1.js
+++ src/Components/ProjectDescrip1.js
@@ -1,17 +1,6 @@
 // Child Component
-const ProjectDescrip1 = ({ heading, links }) => {
-    return (
-        <div className="ProjectDescrip">
-            <h2>{heading}</h2>
-            <ul>
-                {links.map((link, index) => (
-                    <li key={index}>
-                        <a href={link.url}>{link.text}</a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+const ProjectDescrip1 = () => {
+
 }
 
 
