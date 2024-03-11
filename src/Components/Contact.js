55d6403392c607accc98afc9dd81bc11f3c31060
Saachi Varshney <saachi.varshney@gmail.com>
2024-03-11 00:33:13 -0400

    alskdfjg;alkdfs

---
+++ src/Components/Contact.js
@@ -0,0 +1,15 @@
+
+import { Form, Button } from 'react-bootstrap';
+function Contact() {
+ 
+
+    return (
+        <div className="about about2">
+             <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">;
+            </div>
+        </div>
+        )
+    
+    }
+    export default Contact;
