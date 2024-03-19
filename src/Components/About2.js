1080388c5422bc2293ad558428de57fe59133fa8
Saachi Varshney <saachi.varshney@gmail.com>
2024-03-18 21:26:25 -0400

    im actually gonna kms it's gonna happen

--- src/Components/About2.js
+++ src/Components/About2.js
@@ -6,9 +6,9 @@ function About2() {
  
 
     return (
-        <div ref={myRef} className={`about about2 ${myElementIsVisible ? 'animateTestAbout' : ''}`}>
+        <div  className={`about about2 ${myElementIsVisible ? 'animateTestAbout' : ''}`}>
             <p style={{fontSize: '35px', marginBottom: '30px'}}>skills</p>
-            <div id="skillsBox"  style={{fontSize: '40px', }}>
+            <div  ref={myRef} id="skillsBox"  style={{fontSize: '40px', }}>
                <p  style={{fontSize: '40px', margin: '0px' }}>HTML   |   CSS    |   JavaScript    |   Java   |   C</p>
             </div>
             </div>
