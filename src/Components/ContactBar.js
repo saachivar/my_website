c22f45e9a44c3cd5f81ad01ae6e13a5100811a43
Saachi Varshney <saachi.varshney@gmail.com>
2024-03-23 18:30:00 -0400

    im tired

--- src/Components/ContactBar.js
+++ src/Components/ContactBar.js
@@ -4,18 +4,22 @@ import "../images/linkedin.png"
 function ContactBar() {
     return (
         <div className="ContactBar">
-            <button className="ContactBarButton"> 
-                <img className="ContactBarImg" src={ require ('../images/email.png')} alt="email" 
-                style={{ marginRight: "-6px", height: "70px", marginTop: "-14px"}}></img>
-            </button>
-            <button className="ContactBarButton"> 
-                <img className="ContactBarImg" src={ require ('../images/linkedin.png')} alt="linkedin" ></img>
-            </button>
-            <button className="ContactBarButton"> 
-                <img className="ContactBarImg" src={ require ('../images/github-mark.png')} alt="github" ></img>
-            </button>
+            <div className="ContactBarButton"> 
+                <a href="mailto:saachi.varshney@gmail.com" target="_blank" rel="noreferrer">
+                    <img className="ContactBarImg" src={require('../images/email.png')} alt="email" style={{ marginRight: "-6px", height: "70px", marginTop: "-14px"}} />
+                </a>
+            </div>
+            <div className="ContactBarButton"> 
+                <a href="https://www.linkedin.com/in/saachi-varshney-1a13241a5/" target="_blank" rel="noreferrer">
+                    <img className="ContactBarImg" src={require('../images/linkedin.png')} alt="linkedin" />
+                </a>
+            </div>
+            <div className="ContactBarButton"> 
+                <a href="https://github.com/saachivar" target="_blank" rel="noreferrer">
+                    <img className="ContactBarImg" src={require('../images/github-mark.png')} alt="github" />
+                </a>
+            </div>
         </div>
-
     )
 }
 
